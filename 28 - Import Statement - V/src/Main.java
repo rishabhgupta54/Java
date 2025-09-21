c5dacfdf3ebc5ab9366be63a29509d320f3da7e5
Rishabh Gupta <programmer.rishabh@gmail.com>
2025-09-22 00:52:18 +0530

    Java

--- 28 - Import Statement - V/src/Main.java
+++ 28 - Import Statement - V/src/Main.java
@@ -3,6 +3,10 @@ import java.sql.*;
 
 /*
  * Import Statement - V
+ * Ambiguity with Import Statements:
+ * When we import two classes with the same name from different packages (e.g., java.util.Date and java.sql.Date),
+ * the compiler cannot decide which class to use. This results in a compile-time error:
+ * reference to Date is ambiguous
  */
 public class Main {
     public static void main(String[] args) {
