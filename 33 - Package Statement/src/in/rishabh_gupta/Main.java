59a696c897dc2c41b2603711569748d5a82d6541
Rishabh Gupta <programmer.rishabh@gmail.com>
2025-09-22 17:16:09 +0530

    Java

---
+++ 33 - Package Statement/src/in/rishabh_gupta/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Package Statement
+ * It is  an encapsulation mechanism to group related classes and interfaces into a single unit which is nothing but package
+ * examples
+ * - all classes and interfaces which are required for database operations are grouped into a single package which is nothing but java.sql package
+ * - all classes and interfaces which are required for file operations are grouped into a single package which is nothing but java.io package
+ * The main advantages of packages are
+ * - to resolve naming conflicts
+ * - it improves modularity of the application
+ * - it improves maintainability of the application
+ * - There is one universally accepted naming convention i.e. to use internet domain name in reverse order
+ */
+package in.rishabh_gupta;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Package Demo");
+    }
+}
