0eb943bcc217d3de107e9f030757e94272263a44
Rishabh Gupta <programmer.rishabh@gmail.com>
2025-10-02 20:47:58 +0530

    Java

--- 122 - Try Catch - III/src/Main.java
+++ 122 - Try Catch - III/src/Main.java
@@ -1,20 +1,23 @@
 /*
  * Try Catch
- * Throwable class define the following methods to print the exception information
- * - printStackTrace() - Name of the exception:Description
- *   Stack Trace
- * - toString() - Name of the exception:Description
- * - getMessage() - Description
- * Internally default exception handler will use printStackTrace() method to print exception information to the console
+ * - The Throwable class defines the following methods to print exception information:
+ *      1. printStackTrace()
+ *         - Prints the name of the exception, description, and the complete stack trace.
+ *      2. toString()
+ *         - Prints the name of the exception and its description.
+ *      3. getMessage()
+ *         - Prints only the description of the exception.
+ * - Internally, the JVM’s default exception handler uses printStackTrace() to display exception details on the console.
  */
+
 public class Main {
     public static void main(String[] args) {
         try {
-            System.out.println(10 / 0);
+            System.out.println(10 / 0); // risky code
         } catch (Exception exception) {
-            exception.printStackTrace();
-            System.out.println(exception.toString()); // => System.out.println(exception);
-            System.out.println(exception.getMessage());
+            exception.printStackTrace(); // Name + Description + Stack Trace
+            System.out.println(exception.toString()); // Same as System.out.println(exception);
+            System.out.println(exception.getMessage()); // Only description
         }
     }
 }
