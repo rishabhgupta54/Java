69f2412f9b2d57f9cfe1172089019f2cd7469380
Rishabh Gupta <programmer.rishabh@gmail.com>
2025-10-03 23:12:56 +0530

    Java

--- 129 - Throw - IV/src/Main.java
+++ 129 - Throw - IV/src/Main.java
@@ -3,17 +3,20 @@
  * - The throw keyword can be used only with objects of type Throwable or its subclasses.
  * - If we try to use throw with a normal Java object (not a Throwable type),
  *   we will get a compile-time error: "incompatible types".
+ *
+ * Example 1 (Invalid):
+ * class Main {
+ *     public static void main(String[] args) {
+ *         throw new Main(); // Compile-time error: incompatible types
+ *     }
+ * }
+ *
+ * Example 2 (Valid):
+ * - If the class extends Throwable (directly or indirectly), then it can be thrown.
  */
-
-/*public class Main {
-    public static void main(String[] args) {
-        // Invalid: Main is not a subclass of Throwable
-        throw new Main(); // Compile-time error: incompatible types
-    }
-}*/
-
 public class Main extends RuntimeException {
     public static void main(String[] args) {
-        throw new Main(); // Compile-time error: incompatible types
+        // Valid, because Main is a subclass of RuntimeException (which extends Throwable)
+        throw new Main();
     }
 }
